ac65599678e9b7549f158e706fd39e24459a5c47
cvdsoftware <chris@cvdsoftware.nl>
2024-11-08 10:29:42 +0100

    Call Gitea API

--- src/main.ts
+++ src/main.ts
@@ -45,6 +45,24 @@ const run = async (): Promise<void> => {
     if (process.env['GITEA_ACTIONS']) {
         console.log('This is a Gitea Action');
         console.log('token: ' + token)
+
+        // test, for now
+        let url = process.env['GITHUB_API_URL'] + "/repos/" + process.env['GITHUB_REPOSITORY'] + '/issues/43/comments';
+
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': token,
+                'accept': 'application/json'
+            },
+            body: JSON.stringify({ body: comment }),
+        });
+
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
     } else {
         await setSummary(summary);
         await publishComment(token, title, comment, postNewComment);
